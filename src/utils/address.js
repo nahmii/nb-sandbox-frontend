f4cc2167958d28bc75446a919d63e83f619c5f38
Hein <heindauven@gmail.com>
2022-04-05 23:42:13 +0200

    Add address shorten function.

--- src/utils/address.js
+++ src/utils/address.js
@@ -2,13 +2,30 @@ import { getAddress } from "ethers/lib/utils";
 
 /**
  * Returns a checksummed Ethereum address if valid, else returns false.
- * @param {string} address 
+ * @param {string} address Addres to test the validity of.
  * @returns Checksummed address or false if invalid.
  */
-export function isAddress(address) {
+export const isAddress = (address) => {
     try {
         return getAddress(address);
     } catch {
         return false;
     }
+}
+
+/**
+ * Shortens a given hash or string.
+ * @param {string} address - The value to shorten.
+ * @param {number} [start=4] - The first n characters to take from the string.
+ * @param {number} [end=4] - The last n characters to take from the string.
+ * @returns 
+ */
+export const shortenAddress = (address, start = 4, end = 4) => {
+    const parsed = isAddress(address);
+
+    if (!parsed) {
+        throw Error(`Invalid address parameter: ${address}`);
+    }
+
+    return `${parsed.slice(0, start + 2)}...${parsed.slice(-Math.abs(end))}`
 }
