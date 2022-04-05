13c8e6f8bd5b41bf0e9a1de478ccfb50797e15c9
Hein <heindauven@gmail.com>
2022-04-05 22:51:33 +0200

    Add contract instantiation function.

--- src/hooks/useContract.js
+++ src/hooks/useContract.js
@@ -1,1 +1,8 @@
+import TOKEN_ABI from "../abis/cb-token.json";
+import { TOKEN_ADDRESS } from "../constants";
 import { useEffect, useState } from "react";
+import { Contract } from 'ethers';
+
+export const getContract = async (address, ABI, provider) => {
+    return new Contract(address, ABI, provider);
+}
