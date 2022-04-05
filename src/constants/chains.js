ddba36f7ef49112014f7e3106cb02cd97f0bc2f1
Hein <heindauven@gmail.com>
2022-04-05 12:09:01 +0200

    Simplify network constant.

--- src/constants/chains.js
+++ src/constants/chains.js
@@ -1,9 +1,7 @@
-export const SUPPORTED_NETWORKS = [
-    {
+export const SUPPORTED_NETWORK = {
         chainId: "0x6C1",
         chainName: "Bergen",
         blockExplorerUrl: "https://blockscout.bergen.nahmii.io/",
         rpcUrl: "https://rpc.bergen.nahmii.io",
         token: "0x025729a63396A63eDDd50ffB6c5A36681eBe4fdB"
-    }
-]
+}
