12d57b487fcc2fa5baab83a641167e4ac86526fa
Hein <heindauven@gmail.com>
2022-04-05 09:59:51 +0200

    Add chain information constant.

---
+++ src/constants/chains.js
@@ -0,0 +1,8 @@
+export const SUPPORTED_NETWORKS = [
+    {
+        chainId: "0x6C1",
+        chainName: "Bergen",
+        blockExplorerUrl: "https://blockscout.bergen.nahmii.io/",
+        rpcUrl: "https://rpc.bergen.nahmii.io"
+    }
+]
