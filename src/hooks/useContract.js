17be5202ae3d5ff005719d7e861672873dc6c194
Hein <heindauven@gmail.com>
2022-04-05 22:51:56 +0200

    Add get token supply function.

--- src/hooks/useContract.js
+++ src/hooks/useContract.js
@@ -6,3 +6,9 @@ import { Contract } from 'ethers';
 export const getContract = async (address, ABI, provider) => {
     return new Contract(address, ABI, provider);
 }
+
+export const getTokenSupply = async (provider) => {
+    const contract = await getContract(TOKEN_ADDRESS, TOKEN_ABI, provider);
+    const supply = await contract.totalSupply();
+    return supply;
+}
