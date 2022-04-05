222890151fc5b037fc3175de93c485bfeb1104e4
Hein <heindauven@gmail.com>
2022-04-06 00:25:01 +0200

    Add useContract hook. Add TODO for provider data populating.

--- src/hooks/useContract.js
+++ src/hooks/useContract.js
@@ -1,7 +1,23 @@
 import TOKEN_ABI from "../abis/cb-token.json";
 import { TOKEN_ADDRESS } from "../constants";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { getContract } from "../utils/contract";
+
+export const useContract = (address, ABI) => {
+    // TODO: Retrieve provider, account and chainId from hook
+    const { provider, account, chainId } = null;
+
+    return useMemo(() => {
+        if (!address || !ABI || !provider || !chainId) return null;
+
+        try {
+            return getContract(address, ABI, provider);
+        } catch (error) {
+            console.error("Contract retrieval error: ", error);
+            return null;
+        }
+    }, [address, ABI, provider, chainId, account]);
+}
 
 export const getTokenSupply = async (provider) => {
     const contract = await getContract(TOKEN_ADDRESS, TOKEN_ABI, provider);
