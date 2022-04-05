8a965d4854aba9c7b2ffc855497acfa81bca151d
Hein <heindauven@gmail.com>
2022-04-06 00:10:58 +0200

    Use getContract util function.

--- src/hooks/useContract.js
+++ src/hooks/useContract.js
@@ -1,11 +1,7 @@
 import TOKEN_ABI from "../abis/cb-token.json";
 import { TOKEN_ADDRESS } from "../constants";
 import { useEffect, useState } from "react";
-import { Contract } from 'ethers';
-
-export const getContract = async (address, ABI, provider) => {
-    return new Contract(address, ABI, provider);
-}
+import { getContract } from "../utils/contract";
 
 export const getTokenSupply = async (provider) => {
     const contract = await getContract(TOKEN_ADDRESS, TOKEN_ABI, provider);
