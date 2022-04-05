f4cc8fe818e24f1c878c97c23aa0297498d351ad
Hein <heindauven@gmail.com>
2022-04-05 22:35:03 +0200

    Separate token from chain details, rename file appropriately.

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,6 +2,7 @@ export const SUPPORTED_NETWORK = {
         chainId: "0x6C1",
         chainName: "Bergen",
         blockExplorerUrl: "https://blockscout.bergen.nahmii.io/",
-        rpcUrl: "https://rpc.bergen.nahmii.io",
-        token: "0x025729a63396A63eDDd50ffB6c5A36681eBe4fdB"
+        rpcUrl: "https://rpc.bergen.nahmii.io"
 }
+
+export const TOKEN_ADDRESS = "0x025729a63396A63eDDd50ffB6c5A36681eBe4fdB";
