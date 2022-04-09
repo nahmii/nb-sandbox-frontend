6ed7bb85bbc6097e082b6cdf3846af3215517a50
Hein <heindauven@gmail.com>
2022-04-09 16:45:09 +0200

    Remove 0x prefix from state.

--- src/state/index.js
+++ src/state/index.js
@@ -1,7 +1,7 @@
 import { createGlobalState } from 'react-hooks-global-state';
 
 const { setGlobalState, useGlobalState } = createGlobalState({
-    account: '0x',
+    account: '',
     balance: '0.0000',
     totalSupply: '0.0000',
 });
