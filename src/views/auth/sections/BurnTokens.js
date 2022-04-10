3d2c03d0f335ab2eaf19bc547e81d6ad887bcb32
Hein <heindauven@gmail.com>
2022-04-10 19:27:18 +0200

    Refactor burn tokens.

--- src/views/auth/sections/BurnTokens.js
+++ src/views/auth/sections/BurnTokens.js
@@ -1,21 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 import { Card, Box, CardContent, Typography, TextField, InputAdornment, CircularProgress, Snackbar } from '@mui/material'
 import MuiAlert from '@mui/material/Alert'
 import Button from '../../../components/elements/Button'
-import { burnTokens, getContractOwner } from '../../../hooks/useContract';
-import { parseUnits } from 'ethers/lib/utils';
-import { limitDecimalPlaces } from '../../../utils/format';
-import { updateBalance, updateTotalSupply, useGlobalState } from '../../../state';
+import { burnTokens, getContractOwner } from '../../../hooks/useContract'
+import { parseUnits } from 'ethers/lib/utils'
+import { limitDecimalPlaces } from '../../../utils/format'
+import { updateBalance, updateTotalSupply, useGlobalState } from '../../../state'
 
 const cardStyle = {
-    boxShadow: 0, 
+    boxShadow: 0,
     borderRadius: 0,
 }
 
 const inputProps = {
-    backgroundColor: "#F2F8FA", 
-    border: "0px", 
-    height: "50px",
+    backgroundColor: '#F2F8FA',
+    border: '0px',
+    height: '50px',
     ariaLabel: 'weight',
 }
 
@@ -32,13 +32,13 @@ const BurnTokens = () => {
     }
 
     const [account] = useGlobalState('account')
-    const [amountToBurn, setAmountToBurn] = useState('0.0000');
-    const [msg, setMsg] = useState("")
+    const [amountToBurn, setAmountToBurn] = useState('0.0000')
+    const [msg, setMsg] = useState('')
     const [success, setSuccess] = useState(false)
     const [, setError] = useState(false)
     const [loading, setLoading] = useState(false)
     const [disableBtn, setDisableBtn] = useState(false)
-    const [burnBtnText, setBurnBtnText] = useState("BURN TOKENS")
+    const [burnBtnText, setBurnBtnText] = useState('BURN TOKENS')
 
     const handleClick = async () => {
         try {
@@ -46,7 +46,7 @@ const BurnTokens = () => {
             if (amountToBurn < 1) {
                 setOpen(true)
                 setError(true)
-                setMsg("Cannot burn 0 tokens.")
+                setMsg('Cannot burn 0 tokens.')
             } else if (owner.toLowerCase() !== account.toLowerCase()) {
                 setOpen(true)
                 setError(true)
@@ -54,19 +54,19 @@ const BurnTokens = () => {
             } else {
                 setLoading(true)
                 setDisableBtn(true)
-                setBurnBtnText("BURNING TOKENS")
+                setBurnBtnText('BURNING TOKENS')
 
                 const transactionResponse = await burnTokens(parseUnits(amountToBurn, 4))
                 await transactionResponse.wait()
-                
+
                 setOpen(true)
                 setSuccess(true)
                 setMsg(`Burned ${amountToBurn} tokens successfully!`)
-                updateBalance();
-                updateTotalSupply();
-                setLoading(false);
-                setDisableBtn(false);  
-                setBurnBtnText("BURN TOKENS")
+                updateBalance()
+                updateTotalSupply()
+                setLoading(false)
+                setDisableBtn(false)
+                setBurnBtnText('BURN TOKENS')
             }
         } catch (e) {
             console.error(e)
@@ -77,15 +77,15 @@ const BurnTokens = () => {
     }
 
     const handleChange = (event) => {
-        setAmountToBurn(event.target.value);
+        setAmountToBurn(event.target.value)
     }
 
     const handleInput = (event) => {
-        limitDecimalPlaces(event, 4);
+        limitDecimalPlaces(event, 4)
     }
 
     const Alert = React.forwardRef(function Alert(props, ref) {
-        return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />
+        return <MuiAlert elevation={6} ref={ref} variant='filled' {...props} />
     })
 
     return (
@@ -104,33 +104,33 @@ const BurnTokens = () => {
                 </Alert>
             </Snackbar>
             <CardContent>
-                <Typography variant="h6" color="text.secondary" sx={{ fontSize: 18, color: "#153443" }}>
+                <Typography variant='h6' color='text.secondary' sx={{ fontSize: 18, color: '#153443' }}>
                     BURN TOKENS
                 </Typography>
                 <Box
-                    component="form"
+                    component='form'
                     sx={{
-                    '& .MuiTextField-root': { width: '100%' },
+                        '& .MuiTextField-root': { width: '100%' },
                     }}
                     noValidate
-                    autoComplete="off"
-                    style={{marginTop: "20px", marginBottom: "20px"}}
+                    autoComplete='off'
+                    style={{ marginTop: '20px', marginBottom: '20px' }}
                 >
                     <TextField
-                        label="Amount"
-                        id="outlined-start-adornment"
+                        label='Amount'
+                        id='outlined-start-adornment'
                         sx={{ width: '100%' }}
                         value={amountToBurn}
                         onChange={handleChange}
                         onInput={handleInput}
                         InputProps={{
                             type: 'number',
-                            endAdornment: <InputAdornment position="end">NOK</InputAdornment>,
+                            endAdornment: <InputAdornment position='end'>NOK</InputAdornment>,
                             style: inputProps
                         }}
                     />
                 </Box>
-                <Button disabled={disableBtn} style={{color: "white"}} className="button button-primary button-wide-mobile" wide onClick={handleClick}>{burnBtnText} {loading && <CircularProgress sx={{color: "white", padding: "5px", marginBottom: "5px"}}/>}</Button>
+                <Button disabled={disableBtn} style={{ color: 'white' }} className='button button-primary button-wide-mobile' wide onClick={handleClick}>{burnBtnText} {loading && <CircularProgress sx={{ color: 'white', padding: '5px', marginBottom: '5px' }} />}</Button>
             </CardContent>
         </Card>
     )
