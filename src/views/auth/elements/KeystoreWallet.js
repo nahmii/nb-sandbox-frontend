1598e613349be04dcc88a6b62b55f70451e73668
Hein <heindauven@gmail.com>
2022-04-25 09:14:33 +0200

    Format KeystoreWallet element.

--- src/views/auth/elements/KeystoreWallet.js
+++ src/views/auth/elements/KeystoreWallet.js
@@ -1,10 +1,10 @@
-import React, { useEffect, useState } from 'react';
-import { Stepper, Card, Box, Typography, Step, StepButton } from '@mui/material';
+import React, { useState } from 'react'
+import { Stepper, Card, Box, Typography, Step, StepButton } from '@mui/material'
 import HighlightOffIcon from '@mui/icons-material/HighlightOff'
-import SelectFile from '../components/SelectFile';
-import EnterPassword from '../components/EnterPassword';
-import { ethers } from 'ethers';
-import { setGlobalState, useGlobalState } from '../../../state';
+import SelectFile from '../components/SelectFile'
+import EnterPassword from '../components/EnterPassword'
+import { ethers } from 'ethers'
+import { setGlobalState, useGlobalState } from '../../../state'
 
 const steps = [1, 2]
 
@@ -31,7 +31,7 @@ const KeystoreWallet = (props) => {
 
     const onReceiveFile = (text) => {
         try {
-            const parsedFile = JSON.parse(text)
+            const _parsedFile = JSON.parse(text)
             setEncryptedWallet(text)
             setActiveStep(1)
             // TODO: Retrieve address from cipher.
@@ -58,45 +58,45 @@ const KeystoreWallet = (props) => {
 
     return (
         <Card className=''>
-                <Box sx={{ mt: 2, p: 2, borderBottom: "1px solid #CBE5EE"}}>
-                    <Typography id='modal-modal-title' variant='p' sx={{ pl: 2, fomtSize: "12px" }}>
-                        ACCESS WALLET WITH KEYSTORE FILE <span style={{ float: 'right' }}><HighlightOffIcon onClick={onClose} /></span>
-                    </Typography>
+            <Box sx={{ mt: 2, p: 2, borderBottom: '1px solid #CBE5EE' }}>
+                <Typography id='modal-modal-title' variant='p' sx={{ pl: 2, fomtSize: '12px' }}>
+                    ACCESS WALLET WITH KEYSTORE FILE <span style={{ float: 'right' }}><HighlightOffIcon onClick={onClose} /></span>
+                </Typography>
+            </Box>
+            <Box sx={{ mt: 3 }}>
+                <Stepper nonLinear activeStep={activeStep} alternativeLabel>
+                    {steps.map((label, index) => (
+                        <Step key={label} completed={completed[index]}>
+                            <StepButton color='#0078A0'>
+                            </StepButton>
+                        </Step>
+                    ))}
+                </Stepper>
+                <Box sx={{}}>
+                    {allStepsCompleted() ? (
+                        <React.Fragment>
+                            <p>All finished</p>
+                        </React.Fragment>
+                    ) : (
+                        <React.Fragment>
+                            <Typography variant='p' color='text.secondary' sx={{ mt: 2, pl: 3, fontSize: '12px' }}>Step {activeStep + 1}</Typography>
+                            <Box>
+                                {(() => {
+                                    switch (activeStep) {
+                                        case 0:
+                                            return <SelectFile onReceiveFile={onReceiveFile} onBack={onBack} error={isFileError} setError={setFileError} />
+                                        case 1:
+                                            return <EnterPassword onDecryptWallet={onDecryptWallet} error={isPasswordWrong} setError={setIsPasswordWrong} onBack={onBack} />
+                                        default:
+                                            return null
+                                    }
+                                })()}
+                            </Box>
+                        </React.Fragment>
+                    )}
                 </Box>
-                <Box sx={{ mt: 3 }}>
-                    <Stepper nonLinear activeStep={activeStep} alternativeLabel>
-                        {steps.map((label, index) => (
-                            <Step key={label} completed={completed[index]}>
-                                <StepButton color='#0078A0'>
-                                </StepButton>
-                            </Step>
-                        ))}
-                    </Stepper>
-                    <Box sx={{}}>
-                        {allStepsCompleted() ? (
-                            <React.Fragment>
-                                <p>All finished</p>
-                            </React.Fragment>
-                        ) : (
-                            <React.Fragment>
-                                <Typography variant='p' color='text.secondary' sx={{ mt: 2, pl: 3, fontSize: '12px' }}>Step {activeStep + 1}</Typography>
-                                <Box>
-                                    {(() => {
-                                        switch (activeStep) {
-                                            case 0:
-                                                return <SelectFile onReceiveFile={onReceiveFile} onBack={onBack} error={isFileError} setError={setFileError} />
-                                            case 1:
-                                                return <EnterPassword onDecryptWallet={onDecryptWallet} error={isPasswordWrong} setError={setIsPasswordWrong} onBack={onBack} />
-                                            default:
-                                                return null
-                                        }
-                                    })()}
-                                </Box>
-                            </React.Fragment>
-                        )}
-                    </Box>
-                </Box>
-            </Card>
+            </Box>
+        </Card>
     )
 }
 
