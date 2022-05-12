aa2844d4236e63a63a39251cc36dacd38b0fe635
Hein <heindauven@gmail.com>
2022-05-13 00:48:45 +0200

    Wrap the app in an intl provider with the users preferred lang set or default to en.
    (cherry picked from commit 01c2a214fcf62d56f132a7f4df640f44e4700e57)

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import AppRoute from './routes'
 import { retrieveItem } from './utils/localStorage'
 import { setGlobalState, useGlobalState } from './state'
 import { Backdrop, CircularProgress } from '@mui/material'
+import { IntlProvider } from 'react-intl'
+import { getLang } from './utils/intl'
 
 const THEME = createTheme({
     typography: {
@@ -27,17 +29,19 @@ const App = () => {
     }, [])
 
     return (
-        <ThemeProvider theme={THEME}>
-            {loading ? (
-                <Backdrop
-                    sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
-                    open={loading}
-                >
-                    <CircularProgress color="inherit" />
-                </Backdrop>
-            ) : null}
-            <AppRoute />
-        </ThemeProvider>
+        <IntlProvider locale={getLang()} defaultLocale='en'>
+            <ThemeProvider theme={THEME}>
+                {loading ? (
+                    <Backdrop
+                        sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+                        open={loading}
+                    >
+                        <CircularProgress color="inherit" />
+                    </Backdrop>
+                ) : null}
+                <AppRoute />
+            </ThemeProvider>
+        </IntlProvider>
     )
 }
 
