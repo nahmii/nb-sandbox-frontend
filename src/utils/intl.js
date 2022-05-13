b2ebb1413f0739ba6c9c7b29cc649125b860e682
Hein <heindauven@gmail.com>
2022-05-13 10:58:15 +0200

    Add documentation to the getLang function.
    (cherry picked from commit dcdfb208a87d3570f1ae567284a69815eeed6b54)

--- src/utils/intl.js
+++ src/utils/intl.js
@@ -1,3 +1,8 @@
+/**
+ * Function that retrieves the language preference for an agent/user.
+ * It forces a Norwegian locale IF at least one of them is detected in the agents/users language preference list.
+ * @returns Returns the agents preferred language.
+ */
 export const getLang = () => {
     if (navigator.languages !== undefined) {
         if (navigator.languages.includes('nb') ||
