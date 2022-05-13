e21fb4ebe9ef42ad535162f85c64e8b0cfe8a02c
Hein <heindauven@gmail.com>
2022-05-13 10:56:22 +0200

    Check if a users languages list contains a Norwegian intl code. If so, force it to the 'nb' code.
    (cherry picked from commit f4358011344c37f4e02f42b3594a8c2f3c024726)

--- src/utils/intl.js
+++ src/utils/intl.js
@@ -1,5 +1,12 @@
 export const getLang = () => {
     if (navigator.languages !== undefined) {
+        if (navigator.languages.includes('nb') ||
+            navigator.languages.includes('nn') ||
+            navigator.languages.includes('nb-NO') ||
+            navigator.languages.includes('nn-NO') ||
+            navigator.languages.includes('se-NO')) {
+                return 'nb'
+            }
         return navigator.languages[0]
     }
     return navigator.language
