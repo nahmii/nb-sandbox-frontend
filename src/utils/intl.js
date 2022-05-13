809ae2be1ed44b7fb4e5ab3ca85591bc192e0e01
Hein <heindauven@gmail.com>
2022-05-13 11:20:50 +0200

    Check for extra codes in the getLang function, only force the 'nb' locale if the first detected preferred language is Norwegian.
    (cherry picked from commit 7cd94e3e0fb12d75a605932ddbaa81ed0087da29)

--- src/utils/intl.js
+++ src/utils/intl.js
@@ -1,17 +1,19 @@
 /**
  * Function that retrieves the language preference for an agent/user.
- * It forces a Norwegian locale IF at least one of them is detected in the agents/users language preference list.
+ * It forces a Norwegian locale IF the first language detected in the agents/users language preference list is Norwegian/Sami.
  * @returns Returns the agents preferred language.
  */
 export const getLang = () => {
     if (navigator.languages !== undefined) {
-        if (navigator.languages.includes('nb') ||
-            navigator.languages.includes('nn') ||
-            navigator.languages.includes('nb-NO') ||
-            navigator.languages.includes('nn-NO') ||
-            navigator.languages.includes('se-NO')) {
-                return 'nb'
-            }
+        if (navigator.languages[0].includes('nb') ||
+            navigator.languages[0].includes('nn') ||
+            navigator.languages[0].includes('se') ||
+            navigator.languages[0].includes('nb-NO') ||
+            navigator.languages[0].includes('nn-NO') ||
+            navigator.languages[0].includes('se-NO') ||
+            navigator.languages[0].includes('nb-SJ')) {
+            return 'nb'
+        }
         return navigator.languages[0]
     }
     return navigator.language
